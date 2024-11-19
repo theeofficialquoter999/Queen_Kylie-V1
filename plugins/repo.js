1976c8f450f914e376673f4a3fbdbaef8f47a064
errrbodyhatescylee <errr.bodyhatescylee@gmail.com>
2024-11-19 16:15:14 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -14,15 +14,15 @@ smd({
     const response = await axios.get("https://api.github.com/repos/hakisolos/Queen_Nikka");
     const repoData = response.data;
 
-    let replyMessage = `**🍀WhatsApp Bot Repository Info**\n\n`;
-    replyMessage += `**🍀Repository Name:** ${repoData.name}\n`;
-    replyMessage += `**🍀Description:** ${repoData.description || 'No description available'}\n`;
-    replyMessage += `**🍀Stars:** ${repoData.stargazers_count}\n`;
-    replyMessage += `**🍀Forks:** ${repoData.forks_count}\n`;
-    replyMessage += `**🍀Watchers:** ${repoData.watchers_count}\n`;
-    replyMessage += `**🍀Open Issues:** ${repoData.open_issues_count}\n`;
-    replyMessage += `**🍀License:** ${repoData.license ? repoData.license.name : 'No License'}\n`;
-    replyMessage += `**🍀Repository URL:** [Click here]👉(https://github.com/hakisolos/Queen_Nikka)\n`;
+    let replyMessage = `**🧸WhatsApp Bot Repository Info**\n\n`;
+    replyMessage += `**👸Repository Name:** ${repoData.name}\n`;
+    replyMessage += `**✍️Description:** ${repoData.description || 'No description available'}\n`;
+    replyMessage += `**✨Stars:** ${repoData.stargazers_count}\n`;
+    replyMessage += `**🍴Forks:** ${repoData.forks_count}\n`;
+    replyMessage += `**👀Watchers:** ${repoData.watchers_count}\n`;
+    replyMessage += `**🗃️Open Issues:** ${repoData.open_issues_count}\n`;
+    replyMessage += `**💳License:** ${repoData.license ? repoData.license.name : 'No License'}\n`;
+    replyMessage += `**📂Repository URL:** [Click here]👉(https://github.com/errrbodyhatescylee/Queen_Kylie-V1)\n`;
 
     await message.send(replyMessage);
   } catch (error) {
